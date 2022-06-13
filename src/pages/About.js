cae2240312c8e15d79be57d274915d0bc0ec7bb9
DylanBoucher <dylanboucher99@gmail.com>
2022-06-12 20:20:49 -0500

    added some to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,9 @@ const About = () => {
   return (
     <>
     <div>
-      <p className='about-info'>Find resources dedicated to people with special needs.</p>
+      <p className='about-info'>Dedicated to helping you find the best resources and groups related to special needs.</p>
+      <p className='about-info'>Search city, state, or name of a location.</p>
+      <p className='about-info bottom-info'>Find forums and groups across all platforms.</p>
     </div>
     <Footer/>
     </>
